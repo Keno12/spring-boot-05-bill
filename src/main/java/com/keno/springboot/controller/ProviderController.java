2475b673b7f0f7bc3b0bb731d51221e0e1066718
Keno12 <1598136216@qq.com>
2020-01-22 13:25:38 +0800

    项目初始化

---
+++ src/main/java/com/keno/springboot/controller/ProviderController.java
@@ -0,0 +1,12 @@
+package com.keno.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProviderController  {
+    @GetMapping("/providers")
+  public  String list(){
+      return "provider/list";
+  }
+}
